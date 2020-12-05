197ead4bbfe2488760148b6726c86a818cb8ac8b
kkrsnik <kk2697@student.uni-lj.si>
2020-12-05 14:54:48 +0100

    Naloga 6 - Interceptor update

--- storitve/src/main/java/si/fri/prpo/lokacijskiopomniki/storitve/interceptor/BeleziKliceInterceptor.java
+++ storitve/src/main/java/si/fri/prpo/lokacijskiopomniki/storitve/interceptor/BeleziKliceInterceptor.java
@@ -3,17 +3,20 @@ import si.fri.prpo.lokacijskiopomniki.storitve.StevecZrno;
 import si.fri.prpo.lokacijskiopomniki.storitve.anotacije.BeleziKlice;
 import javax.inject.Inject;
 import javax.interceptor.*;
+import java.util.logging.Logger;
 
 @Interceptor
 @BeleziKlice
 public class BeleziKliceInterceptor {
+    private final Logger log = Logger.getLogger(BeleziKliceInterceptor.class.getName());
 
     @Inject
     private StevecZrno steveczrno;
 
     @AroundInvoke
     public Object dobiSteviloKlicev(InvocationContext context) throws Exception{
-        steveczrno.povecajStevec();
+        int st = steveczrno.povecajStevec();
+        log.info("St klicev: " + st);
         return context.proceed();
     }
 
